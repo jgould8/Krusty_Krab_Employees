9e639ca072701cf13e729877e85f2d754379da03
Nathan <nfinney@charlotte.edu>
2024-12-06 22:31:56 -0500

    Eighth commit updated index

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,5 +1,11 @@
-from . import orders
+from . import orders, resources, ratings, promotions, payment, menu_items, customers
 
 
 def load_routes(app):
     app.include_router(orders.router)
+    app.include_router(resources.router)
+    app.include_router(ratings.router)
+    app.include_router(promotions.router)
+    app.include_router(payment.router)
+    app.include_router(menu_items.router)
+    app.include_router(customers.router)
